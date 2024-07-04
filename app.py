892e2385b22cd1a9feb0285b7a55fa0a9d1fb1f9
Mohammed Ali <binbasri.india@gmail.com>
2024-07-04 16:29:50 +0530

    So far

--- app.py
+++ app.py
@@ -4,11 +4,32 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def challenge_one():
-    return render_template('challenge1.html')
+    return render_template('round1/challenge1.html')
 
-@app.route('/secretkey')
+@app.route('/DominatePixelHertz')
 def challenge_two():
-    return render_template('challenge2.html')
+    return render_template('round1/challenge2.html')
+
+@app.route('/round2')
+def round2():
+    return render_template('round2/challenge1.html')
+
+@app.route('/flag21')
+def flag21():
+    return render_template('round2/challenge2.html')
+
+@app.route('/flag22')
+def flag22():
+    return render_template('round2/challenge3.html')
+
+@app.route('/round3')
+def round3():
+    return render_template('round3/challenge1.html')
+
+@app.route('/flag31')
+def flag31():
+    return render_template('round3/challenge2.html')
+
 
 # for 404 error
 @app.errorhandler(404)
