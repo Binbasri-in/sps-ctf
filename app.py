70051c874dc64d56348d80bcd18c85f971f08a0f
Mohammed Ali <binbasri.india@gmail.com>
2024-07-05 11:05:44 +0530

    comment

--- app.py
+++ app.py
@@ -14,29 +14,29 @@ def challenge_two():
 def lunch():
     return render_template('round1/lunch.html')
 
-@app.route('/logic-lock')
-def round2():
-    return render_template('round2/challenge1.html')
+# @app.route('/logic-lock')
+# def round2():
+#     return render_template('round2/challenge1.html')
 
-@app.route('/ctse')
-def flag21():
-    return render_template('round2/challenge2.html')
+# @app.route('/ctse')
+# def flag21():
+#     return render_template('round2/challenge2.html')
 
-@app.route('/gate')
-def flag22():
-    return render_template('round2/challenge3.html')
+# @app.route('/gate')
+# def flag22():
+#     return render_template('round2/challenge3.html')
 
-@app.route('/jawed')
-def flag23():
-    return render_template('round2/final.html')
+# @app.route('/jawed')
+# def flag23():
+#     return render_template('round2/final.html')
 
-@app.route('/extra')
-def round3():
-    return render_template('extra/challenge1.html')
+# @app.route('/extra')
+# def round3():
+#     return render_template('extra/challenge1.html')
 
-@app.route('/extra2')
-def flag31():
-    return render_template('extra/challenge2.html')
+# @app.route('/extra2')
+# def flag31():
+#     return render_template('extra/challenge2.html')
 
 
 # for 404 error
